d4b3546986812a5aae4e3c26ec432ff3b9a6fbaf
Henry Segal <hseg1227@gmail.com>
2021-09-27 10:29:33 -0500

    Fix macOS window hint

--- src/window.cpp
+++ src/window.cpp
@@ -11,7 +11,7 @@ namespace cgull {
         #ifdef __APPLE__
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         #endif
     
