cb7aed38be0593bad9d805c9b4fda3db0a88a08c
Henry Segal <hseg1227@gmail.com>
2021-11-25 14:11:27 -0600

    Convert tabs to spaces

--- src/inputs.cpp
+++ src/inputs.cpp
@@ -4,18 +4,18 @@
 #include <functional>
 
 namespace cgull {
-	namespace key {
+    namespace key {
         key_combination seq(std::initializer_list<key_mods> mods, key_code key) {
-        	unsigned int m = std::transform_reduce(
-        		mods.begin(), mods.end(),
-        		0, std::bit_or<unsigned int>{},
-        		[](key_mods km){return static_cast<unsigned int>(km);}
-        	);
-        	return key_combination(key, m);
+            unsigned int m = std::transform_reduce(
+                mods.begin(), mods.end(),
+                0, std::bit_or<unsigned int>{},
+                [](key_mods km){return static_cast<unsigned int>(km);}
+            );
+            return key_combination(key, m);
         }
 
-		key_combination seq(key_code key) {
-			return key_combination(key, key_mods::none);
-		}
-	}
+        key_combination seq(key_code key) {
+            return key_combination(key, key_mods::none);
+        }
+    }
 }
