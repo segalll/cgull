5686cc0a0531e831d9fe610922f2eb3a0c61458a
Henry Segal <hseg1227@gmail.com>
2022-05-12 11:53:38 -0500

    Remove unnecessary files

--- openprojectdialog.cpp
+++
@@ -1,6 +0,0 @@
-#include "openprojectdialog.h"
-
-OpenProjectDialog::OpenProjectDialog() {
-    setFileMode(QFileDialog::ExistingFile);
-    setNameFilter("CGull project files (*.cgull *.bluej)");
-}
